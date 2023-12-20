b707cc85e45f204724b95bc541bcc17387c07e2c
pajam <payam2001@gmail.com>
2023-12-20 22:21:27 +0100

    commit

--- pipeline.py
+++ pipeline.py
@@ -4,11 +4,13 @@ import sqlite3
 
 # Extract
 ships = pd.read_csv("2019_AIS.csv")
-ports = pd.read_csv("major_ports.csv")
+ports = pd.read_csv("PORTS.csv", delimiter=";")
 
 # Transform
 ships['LATITUDE'] = ships['LATITUDE'].astype(str).str.replace(',', '.').astype(float)
 ships['LONGITUDE'] = ships['LONGITUDE'].astype(str).str.replace(',', '.').astype(float)
+ports['LATITUDE'] = ports['LATITUDE'].astype(str).str.replace(',', '.').astype(float)
+ports['LONGITUDE'] = ports['LONGITUDE'].astype(str).str.replace(',', '.').astype(float)
 
 
 # The following function can be used to find the distance between ships and ports. I will use this to filter emissions occured close to ports. 
@@ -25,10 +27,10 @@ ships['ports_in_range_indexes'] = None
 for index, row in ships.iterrows():
     lng_distance = 0.3
     lat_distance = 0.2
-    condition1 = ports['Lng'] > row['LONGITUDE'] - lng_distance
-    condition2 = ports['Lng'] < row['LONGITUDE'] + lng_distance
-    condition3 = ports['Lat'] > row['LATITUDE'] - lat_distance
-    condition4 = ports['Lat'] < row['LATITUDE'] + lat_distance
+    condition1 = ports['LONGITUDE'] > row['LONGITUDE'] - lng_distance
+    condition2 = ports['LONGITUDE'] < row['LONGITUDE'] + lng_distance
+    condition3 = ports['LATITUDE'] > row['LATITUDE'] - lat_distance
+    condition4 = ports['LATITUDE'] < row['LATITUDE'] + lat_distance
 
     result_indexes = ports[condition1 & condition2 & condition3 & condition4].index
     if result_indexes.empty:
@@ -50,23 +52,29 @@ for index, ship in ships_around_ports.iterrows():
     closest_port_index = None
 
     for value in ship['ports_in_range_indexes']:
-        lat_port = float(ports.loc[value, 'Lat'])
-        lon_port = float(ports.loc[value, 'Lng'])
+        lat_port = float(ports.loc[value, 'LATITUDE'])
+        lon_port = float(ports.loc[value, 'LONGITUDE'])
         distance = calculate_distance(lat_ship, lon_ship, lat_port, lon_port)
 
         if distance < min_distance:
             min_distance = distance
             closest_port_index = value
 
-    ships.at[index, 'closest_port'] = ports.loc[closest_port_index, 'Postal']
-    ships.at[index, 'continent'] = ports.loc[closest_port_index, 'REGION']
+    ships.at[index, 'closest_port'] = ports.loc[closest_port_index, 'PORT_NAME']
+    ships.at[index, 'continent'] = ports.loc[closest_port_index, 'Continent']
     ships.at[index, 'distance_to_closest_port'] = min_distance
         
         
 # Convert lists to strings
 ships['ports_in_range_indexes'] = ships_around_ports['ports_in_range_indexes'].apply(lambda x: str(x))        
 
-      
+ports_emissions = pd.read_sql_query("
+    SELECT s.closest_port, s.continent, SUM(s.E_CO2_kg) AS total_E_CO2_kg, p.LATITUDE, p.LONGITUDE
+    FROM ships s
+    LEFT JOIN ports p ON s.closest_port = p.PORT_NAME
+    WHERE s.closest_port IS NOT NULL
+    GROUP BY s.closest_port
+", con)
         
 # Load
 con = sqlite3.connect("dwh/db")
@@ -74,3 +82,4 @@ con = sqlite3.connect("dwh/db")
 
 ships.to_sql('ships', con, if_exists='replace', index=False)
 ports.to_sql("ports", con, if_exists="replace")
+ports_emissions.to_sql("ports_emissions", con, if_exists='replace')
